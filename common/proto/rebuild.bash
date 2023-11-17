687a71d493870279616358c902ea159c7004cfc6
cn-hideyoshi <645165778@qq.com>
2023-11-17 16:43:28 +0800

    add test proto file

---
+++ common/proto/rebuild.bash
@@ -0,0 +1,5 @@
+if [ $1 ]; then
+  echo "正在生成$1文件下的service"
+  protoc --proto_path=$1  --go_out=../pkg/service/$1.v1/ --go_opt=paths=source_relative --go-grpc_out=../pkg/service/$1.v1/ --go-grpc_opt=paths=source_relative ./$1/*.proto
+  else echo "请收入待生成文件夹"
+fi
